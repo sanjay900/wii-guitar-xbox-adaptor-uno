ec7efc0a900debea1d0b25bcdec4c907956af799
Sanjay Govind <sanjay.govind9@gmail.com>
2019-01-08 02:41:48 +1300

    Remove some duplication

--- ArduinoCode/WiiController.h
+++ ArduinoCode/WiiController.h
@@ -1,1 +1,12 @@
-../WiiController.h
+#pragma once
+#include <inttypes.h>
+typedef struct {
+  uint8_t digital_buttons_1;
+  uint8_t digital_buttons_2;
+  uint8_t lt;
+  uint8_t rt;
+  uint8_t l_x;
+  uint8_t l_y;
+  uint16_t r_x;
+  uint16_t r_y;
+} WiiController;
